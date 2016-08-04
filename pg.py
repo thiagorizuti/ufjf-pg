f520f932a0a95f3b5b40514aa8b90366af557d0e
Thiago Costa Rizuti da Rocha <t.rizuti@gmail.com>
2016-08-04 20:47:05 -0300

    last version

--- pg.py
+++ pg.py
@@ -10,19 +10,26 @@ from deap import base
 from deap import creator
 from deap import tools
 from deap import gp
+from sys import argv
 
-
-random.seed(datetime.now())
-
-with open("ecoli3.csv") as dataset:
+seed = int(argv[1])
+file_name = argv[2]
+if argv[3] == 'True':
+    weighted = True
+else:
+    weighted = False
+random.seed(seed)
+with open(file_name) as dataset:
     reader = csv.reader(dataset)
     dataset = list(list(float(elem) for elem in row) for row in reader)
+att = 8
 class0 = []
 class1 = []
 train = []
 test = []
+
 for line in dataset:
-    if line[7] == 0:
+    if line[att] == 0:
         class0.append(line)
     else:
         class1.append(line)
@@ -32,6 +39,8 @@ random.shuffle(class1)
 
 l0 = len(class0)
 l1 = len(class1)
+
+
 
 for i in range(int(l0*0.1)):
     x = class0.pop(0)
@@ -50,7 +59,7 @@ def safeDiv(a, b):
         return 0
     return a / b
 
-pset = gp.PrimitiveSetTyped("main", [float,float,float,float,float,float,float],bool)
+pset = gp.PrimitiveSetTyped("main", itertools.repeat(float, att),bool)
 pset.addPrimitive(operator.lt, [float, float], bool)
 pset.addPrimitive(operator.le, [float, float], bool)
 pset.addPrimitive(operator.gt, [float, float], bool)
@@ -80,70 +89,67 @@ toolbox.register("compile", gp.compile, pset=pset)
 
 def eval(individual):
     func = toolbox.compile(expr=individual)
-    fit = 0
-    total = 0
+    fit0 = 0
+    fit1 = 0
+    total0 = 0
+    total1 = 0
     for line in train:
-        result = func(float(line[0]),float(line[1]),float(line[2]),float(line[3]),
-        float(line[4]),float(line[5]),float(line[6]))
-        if result and int(line[7]) == 1:
-            fit += 1
-        if not result and int(line[7])  == 0:
-            fit += 1
-        total += 1
-    return  float(fit)/total,
+        result = func(*line[:att])
+        if line[att] == 0:
+            total0 += 1
+            if not result:
+                fit0 += 1
+        if line[att] == 1:
+            total1 += 1
+            if result:
+                fit1 += 1
+    total = total1 + total0
+    w0 = float(total1)/total
+    w1 = float(total0)/total
+    if weighted:
+        return float(fit0*w0+fit1*w1)/total,
+    else:
+        return float(fit0+fit1)/total,
 
 toolbox.register("evaluate", eval)
 toolbox.register("select", tools.selRoulette)
 toolbox.register("mate", gp.cxOnePoint)
 toolbox.register("expr_mut", gp.genGrow, min_=0, max_=2)
 toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
+toolbox.decorate("mate", gp.staticLimit(operator.attrgetter('height'),max_value=15))
 
-def overral_accuracy(individual, dataset):
+def accuracy(individual, dataset):
     func = toolbox.compile(expr=individual)
-    fit = 0
-    total = 0
-    for line in dataset:
-        result = func(float(line[0]),float(line[1]),float(line[2]),float(line[3]),
-        float(line[4]),float(line[5]),float(line[6]))
-        if result and int(line[7]) == 1:
-            fit += 1
-        if not result and int(line[7])  == 0:
-            fit += 1
-        total += 1
-    return  float(fit)/total,
-
-def class_accuracy(individual, dataset):
-    func = toolbox.compile(expr=individual)
-    fit0 = 0
-    fit1 = 0
+    acc0 = 0
+    acc1 = 0
     total0 = 0
     total1 = 0
     for line in dataset:
-        result = func(line[0],line[1],line[2],line[3],
-        line[4],line[5],line[6])
-        if line[7] == 0:
+        result = func(*line[:att])
+        if line[att] == 0:
             total0 += 1
-            if result:
-                fit0 += 1
-        if line[7] == 1:
+            if not result:
+                acc0 += 1
+        if line[att] == 1:
             total1 += 1
             if result:
-                fit1 += 1
-    return  float(fit0)/total0, float(fit1)/total1,
+                acc1 += 1
+    return  float(acc0+acc1)/(total0+total1), safeDiv(float(acc0),total0), safeDiv(float(acc1),total1)
+
 
 def main():
-
     pop = toolbox.population(n=100)
     hof = tools.HallOfFame(1)
-    cxpb = 0.5
-    mutpb = 0.3
-    ngen = 50
-    algorithms.eaSimple(pop, toolbox, cxpb, mutpb,ngen, halloffame=hof)
-
+    cxpb = 0.8
+    mutpb = 0.2
+    ngen = 500
+    algorithms.eaSimple(pop, toolbox, cxpb, mutpb,ngen, halloffame=hof,verbose=True)
     for ind in hof:
         ind1 = ind
-        print class_accuracy(ind,test), overral_accuracy(ind,test)
-       
+        print accuracy(ind,test)
+        print " "
+
+
     return 0
 
 
